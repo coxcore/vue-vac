fa10c23a4adfb890e84b7b088231ad17fff418b6
ascript <ascript0@gmail.com>
2021-10-02 13:18:18 +0900

    initialize vac component

---
+++ src/VAC.vue
@@ -0,0 +1,34 @@
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
+
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VAC",
+  props: {
+    name: String,
+    data: Object | Array | Function | String | Number | Boolean | null,
+    trace: String,
+    listTrace: String,
+    customEvent: Function,
+    hidden: Boolean,
+    maxWidth: Number | String,
+    maxHeight: Number | String,
+    useValue: String,
+    useDefaultValue: String,
+    useList: String,
+    useEach: String,
+  },
+  created() {
+    console.log(this.data);
+  },
+};
+</script>
